3069e894cb3492e3c00eb1a7ca96c6dcdb9feacc
Johnner <jhnner@gmail.com>
2014-04-25 18:47:49 +0400

    upd comment

--- js/background.js
+++ js/background.js
@@ -11,8 +11,8 @@ chrome.contextMenus.create({
 
 /**
  * Can't get chrome.storage directly from content_script
- * so content_script send requset message and background script responds with
- * storage value
+ * so content_script sends request message and then background script
+ * responds with storage value
  */
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if (request.method == "get_fast_option") {
